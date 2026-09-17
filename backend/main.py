47c75c48753968d0f087daaeeabf8d52aa2eb897
temitope-ogunbanjo <pessoal.temitope@gmail.com>
2026-09-17 20:09:50 -0300

    Alterações para rodar o projeto usando uvicorn:
    - .env temporário
    - rout em views para rodar templates do html
    - mudanças na estrutura do projeto para rodar
    
    - Observação: Para rodar via uvicorn precisa estar na past backend. Use o comando cd backend seguido por uvicorn main:app --reload

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,21 @@
+from pathlib import Path
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
 from app.api.router import api_router
+from app.api.routes import views
 from app.db.session import engine
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+FRONTEND_DIR = BASE_DIR / "frontend"
+BACKEND_DIR = BASE_DIR / "backend"
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     yield
     engine.dispose()
-
 
 app = FastAPI(
     title="GoodWe API",
@@ -18,7 +23,14 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
+app.mount("/frontend", StaticFiles(directory=str(FRONTEND_DIR)), name="frontend")
+
 app.include_router(
     api_router,
     prefix="/api",
+)
+
+app.include_router(
+    views.router,
+    tags=["Páginas Web"]
 )
